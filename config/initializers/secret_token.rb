15a0a521286d51120ab8d15ec11634c383c63395
Jonathan Chang <jonoschang7@gmail.com>
2014-05-11 18:44:22 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waysweet::Application.config.secret_key_base = '90d3f5ac666f1485a217039d6a2e792f2af627248fc49ddad936f5e391bbd93923e9431fb1ad6463bdec2479ce69610850c27e95d95ccae5678fdbbc835528bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Waysweet::Application.config.session_store :cookie_store, key: '_waysweet_session'
