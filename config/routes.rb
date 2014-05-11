4995cc9d0f7090c211564f5fb968ef71c24b36f6
Jonathan Chang <jonoschang7@gmail.com>
2014-05-11 19:00:28 -0400

    Route for plans

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Waysweet::Application.routes.draw do
+  resources :plans
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
