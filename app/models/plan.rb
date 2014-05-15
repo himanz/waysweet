b300a8031b6eeb208fb176f87c4c8e75fd7bed63
Jonathan Chang <jonoschang7@gmail.com>
2014-05-15 16:28:33 -0400

    Weight to heatmap works

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,12 @@ class Plan < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   has_many :comments
 
+  def self.average_city_price(cityname)
+    allcities = Plan.where(city_id:cityname.id)
+    plansum = allcities.to_a.sum(&:price)
+    planaverage = plansum / allcities.count
+    return planaverage
+  end
+
+ 
 end
