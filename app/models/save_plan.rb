a66532ab045e1c1fdd5520c7c7d8df3ef0c7e827
Jonathan Chang <jonoschang7@gmail.com>
2014-05-17 23:30:56 -0400

    Create savePlan model

---
+++ app/models/save_plan.rb
@@ -0,0 +1,2 @@
+class SavePlan < ActiveRecord::Base
+end
---
+++ test/models/save_plan_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SavePlanTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
