4623ef40812c7d622b14c8a0a9a7057b1238dc88
Philip Jun <inbox@philipjun.com>
2014-05-21 08:31:18 -0400

    added first and last name for user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
 	  validates_confirmation_of :password
 	  validates_presence_of :password, :on => :create
 	  validates_presence_of :email
+    validates_presence_of :first_name
+    validates_presence_of :last_name
 	  validates_uniqueness_of :email
 
  end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519191027) do
+ActiveRecord::Schema.define(version: 20140521122554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 20140519191027) do
     t.datetime "updated_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
