fac097e1cbcb980595865aeb860b8a6b15ac8acc
Philip Jun <inbox@philipjun.com>
2014-05-22 14:41:52 -0400

    added plans to compare

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,14 +4,13 @@ $(function() {
  $( "#dropplanlist" ).droppable({
   drop: function( event, ui ) {
     $( this )
+    console.log(ui.draggable)
     alert("Plan Added!");
-
+    var plan = $(ui.draggable).clone();
+    $(".planmodal").append(plan);
   }
-});
-});
-
-
-$( "#tooltip" ).click(function() {
-    console.log("test")
   });
+});
+
+
 
