cbe585c5a4cfb4f516afa8feefa8f6be356be64b
Jonathan Chang <jonoschang7@gmail.com>
2014-05-22 13:06:09 -0400

    Error messages for user create form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
 	  validates_confirmation_of :password
 	  validates_presence_of :password, :on => :create
+    validates_presence_of :password_confirmation, :on => :create
 	  validates_presence_of :email
     validates_presence_of :first_name
     validates_presence_of :last_name
