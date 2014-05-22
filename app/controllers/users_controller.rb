a9901a3cd7f9f608c83864efb0c2e3d36a5242e2
Jonathan Chang <jonoschang7@gmail.com>
2014-05-22 15:39:44 -0400

    User login on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      auto_login(@user)
   		redirect_to plans_url, :notice => "Signed up!"
   	else
   		render :new
