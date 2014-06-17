26005e1dffb84e2a8a7ea7a5f8ca0607885cbcb9
Philip Jun <inbox@philipjun.com>
2014-06-17 15:18:53 -0400

    updated admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,27 @@
 class Admin::UsersController < ApplicationController
   before_filter :authorized?
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+  
+
   def authorized?
     unless current_user.has_role? :admin
       flash[:error] = "You are not authorized to view that page."
-      redirect_to root_path
+      redirect_to plans_url
     end
   end 
 end
