3ad30ca49e9f86b23dc4f2a39a1e3c2da1e7cf67
Philip Jun <inbox@philipjun.com>
2014-06-17 21:15:50 -0400

    changed sessions controller to admin view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = login(params[:email], params[:password], params[:remember_me])
-  	if user
-  		redirect_back_or_to plans_url, :notice => "Logged in!"
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+  		redirect_to admin_url, :notice => "Logged in!"
   	else
   		flash.now.alert = "Email or password was invalid"
-  		render :new
+  		
   	end
   end
 
