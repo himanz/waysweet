fcab5be0d68a3969421448bbb4254cc8dc20a06b
Philip Jun <inbox@philipjun.com>
2014-06-19 22:37:58 -0400

    password reset new page

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,16 +1,15 @@
 class PasswordResetsController < ApplicationController
-
   skip_before_filter :require_login
 
-  def create
-
+  # request password reset.
+  # you get here when the user entered his email in the reset password form and submitted it.
+  def create 
     @user = User.find_by_email(params[:email])
 
-# This line sends an email to the user with instructions on how to reset their password (a url with a random token)
+    # This line sends an email to the user with instructions on how to reset their password (a url with a random token)
     @user.deliver_reset_password_instructions! if @user
 
-
-# Tell the user instructions have been sent whether or not email was found.
+    # Tell the user instructions have been sent whether or not email was found.
     # This is to not leak information to attackers about which emails exist in the system.
     redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
   end
@@ -25,7 +24,8 @@ class PasswordResetsController < ApplicationController
       return
     end
   end
- # This action fires when the user has sent the reset password form.
+
+  # This action fires when the user has sent the reset password form.
   def update
     @token = params[:token]
     @user = User.load_from_reset_password_token(params[:token])
@@ -33,8 +33,8 @@ class PasswordResetsController < ApplicationController
     if @user.blank?
       not_authenticated
       return
-  end
-  
+    end
+
     # the next line makes the password confirmation validation work
     @user.password_confirmation = params[:user][:password_confirmation]
     # the next line clears the temporary token and updates the password
@@ -43,5 +43,5 @@ class PasswordResetsController < ApplicationController
     else
       render :action => "edit"
     end
-  end
+   end
 end
