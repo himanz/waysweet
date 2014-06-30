c9c88a2284f076dff706c7a092c64c00a1d9c8f4
Philip Jun <inbox@philipjun.com>
2014-06-29 22:39:55 -0400

    updated sorcery external

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -109,12 +109,12 @@ Rails.application.config.sorcery.configure do |config|
   # config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
   # config.twitter.user_info_mapping = {:email => "screen_name"}
   #
-# config.facebook.key = "<your key here>"
-#   config.facebook.secret = "<your key here>"
-#   config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
-#   config.facebook.user_info_mapping = {:email => "email", :name => "name", :username => "username", :hometown => "hometown/name"} #etc
-#   config.facebook.scope = "email,offline_access,user_hometown,user_interests,user_likes" #etc
-#   config.facebook.display = "popup"
+config.facebook.key = "<your key here>"
+  config.facebook.secret = "<your key here>"
+  config.facebook.callback_url = "http://localhost:3000/oauth/callback?provider=facebook"
+  config.facebook.user_info_mapping = {:email => "email", :name => "name", :username => "username", :hometown => "hometown/name"} #etc
+  config.facebook.scope = "email,offline_access,user_hometown,user_interests,user_likes" #etc
+  config.facebook.display = "popup"
   #
   # config.github.key = ""
   # config.github.secret = ""
