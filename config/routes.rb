78794f80be25c418e74607cf8b99cf60699be7e9
Philip Jun <inbox@philipjun.com>
2014-06-30 12:52:39 -0400

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Waysweet::Application.routes.draw do
 
+
 post "oauth/callback" => "oauths#callback"
 get "oauth/callback" => "oauths#callback" # for use with Github
 get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
