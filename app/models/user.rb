fcd3b5282db93fa2169fe3ed8781aabb4efe659e
Jonathan Chang <jonoschang7@gmail.com>
2014-07-05 16:40:59 -0400

    uncomment out validations, login works again

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,20 @@ class User < ActiveRecord::Base
     config.authentications_class = Authentication
   end
   
-
+  
   has_many :plans_owned, class_name: "Plan", foreign_key: 'owner_id'
   has_many :comments
   has_many :save_plans
   has_many :plans, :through => :save_plans 
   has_many :authentications, :dependent => :destroy
-  
+  # has_secure_password
 
-	  # validates_confirmation_of :password
-	  # validates_presence_of :password, :on => :create
-   #  validates_presence_of :password_confirmation, :on => :create
-	  # validates_presence_of :email
-   #  validates_presence_of :first_name
-   #  validates_presence_of :last_name
+	  validates_confirmation_of :password
+	  validates_presence_of :password, :on => :create
+    validates_presence_of :password_confirmation, :on => :create
+	  validates_presence_of :email
+    validates_presence_of :first_name
+    validates_presence_of :last_name
 	  validates_uniqueness_of :email
 
  end
